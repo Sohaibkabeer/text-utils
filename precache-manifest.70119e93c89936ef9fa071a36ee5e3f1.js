6383fdb8c7a1f1a3a4a694e814d4d468c08a8c71
Sohaibkabeer <sohaibkabeer@gmail.com>
2023-09-10 06:33:53 -0700

    Updates

---
+++ precache-manifest.70119e93c89936ef9fa071a36ee5e3f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a27e18c0071e4a6daf65",
+    "url": "/text-utils/static/css/main.ff1485ab.chunk.css"
+  },
+  {
+    "revision": "a27e18c0071e4a6daf65",
+    "url": "/text-utils/static/js/main.a27e18c0.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/text-utils/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "7a63f99bd12d8c18cd63",
+    "url": "/text-utils/static/js/2.7a63f99b.chunk.js"
+  },
+  {
+    "revision": "879b39b0484c9c05df23",
+    "url": "/text-utils/static/js/runtime~main.879b39b0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/text-utils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5d0c2e8efc19b2449be373696d9ba967",
+    "url": "/text-utils/index.html"
+  }
+];
